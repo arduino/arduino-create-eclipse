6b03dcb3d09c36df22960912ca84f69ce2f819f8
Akos Kitta <kittaakos@typefox.io>
2019-10-11 10:55:21 +0200

    Bare minimum project layout
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

---
+++ bundles/cc.arduino.create.ui/src/cc/arduino/create/ui/importer/wizard/ImportWizard.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package cc.arduino.create.ui.importer.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+/**
+ *
+ */
+public class ImportWizard extends Wizard implements IImportWizard {
+
+    @Override
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean performFinish() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
